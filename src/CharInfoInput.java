63646c6fe7b1545aac9604ce4674ff26c9ba4036
Ian P. Sweeney <Ian.P.Sweeney@students.ittralee.ie>
2019-11-12 17:42:20 +0000

    Signed-off-by: Ian P. Sweeney <Ian.P.Sweeney@students.ittralee.ie>
    Changed char input handling

--- src/CharInfoInput.java
+++ src/CharInfoInput.java
@@ -1,12 +1,10 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class CharInfoInput extends JPanel implements ActionListener {
-    private JFrame charInputGUI = new JFrame("Character Input");
-    private JLabel titleLabel;
-    private JButton confirmButton;
+public class CharInfoInput extends JPanel{
+    private JPanel charInputGUI = new JPanel();
+    private JLabel lblTitle;
+    private JComboBox cboClasses;
+    //private JButton confirmButton;
     private JTextField txtCharName;
     private JTextField txtPlayerName;
     private JTextField txtBackground;
@@ -37,27 +35,29 @@ public class CharInfoInput extends JPanel implements ActionListener {
     private JLabel lblMaxHP;
 
     public CharInfoInput() {
+        String[] classes = { "Wizard", "Sorcerer", "Bard", "Rogue"};
         //Setting GUI size and layout
         charInputGUI.setSize(350, 600);
         charInputGUI.setLayout(null);
-        //Creating title and confirm button
-        titleLabel = new JLabel ("Character Info Input");
-        confirmButton = new JButton ("Confirm Character Input");
+        //Creating title and class combo box
+        lblTitle = new JLabel ("Character Info Input");
+        cboClasses = new JComboBox(classes);
+        //confirmButton = new JButton ("Confirm Character Input");
         //Creating and setting text for all required JLabels
-        lblCharName = new JLabel ("Character Name");
-        lblPlayerName = new JLabel ("Player Name");
-        lblBackground = new JLabel ("Background");
-        lblAlignment = new JLabel("Alignment");
-        lblRace = new JLabel("Race");
-        lblLevel = new JLabel("Level");
-        lblStrength = new JLabel("Strength");
-        lblDexterity = new JLabel("Dexterity");
-        lblConstitution = new JLabel("Constitution");
-        lblIntelligence = new JLabel("Intelligence");
-        lblWisdom = new JLabel("Wisdom");
-        lblCharisma = new JLabel("Charisma");
-        lblMoveSpeed = new JLabel("Movement Speed");
-        lblMaxHP = new JLabel("Max HP");
+        lblCharName = new JLabel ("Character Name", SwingConstants.RIGHT);
+        lblPlayerName = new JLabel ("Player Name", SwingConstants.RIGHT);
+        lblBackground = new JLabel ("Background", SwingConstants.RIGHT);
+        lblAlignment = new JLabel("Alignment", SwingConstants.RIGHT);
+        lblRace = new JLabel("Race", SwingConstants.RIGHT);
+        lblLevel = new JLabel("Level", SwingConstants.RIGHT);
+        lblStrength = new JLabel("Strength", SwingConstants.RIGHT);
+        lblDexterity = new JLabel("Dexterity", SwingConstants.RIGHT);
+        lblConstitution = new JLabel("Constitution", SwingConstants.RIGHT);
+        lblIntelligence = new JLabel("Intelligence", SwingConstants.RIGHT);
+        lblWisdom = new JLabel("Wisdom", SwingConstants.RIGHT);
+        lblCharisma = new JLabel("Charisma", SwingConstants.RIGHT);
+        lblMoveSpeed = new JLabel("Movement Speed", SwingConstants.RIGHT);
+        lblMaxHP = new JLabel("Max HP", SwingConstants.RIGHT);
         //Creating all required JTextFields
         txtCharName = new JTextField (5);
         txtPlayerName = new JTextField (5);
@@ -74,13 +74,10 @@ public class CharInfoInput extends JPanel implements ActionListener {
         txtMoveSpeed = new JTextField();
         txtMaxHP = new JTextField();
         //Adding all the components to the GUI window
-        charInputGUI.add(confirmButton);
-        charInputGUI.add(titleLabel);
-        charInputGUI.add(txtPlayerName);
+        charInputGUI.add(lblTitle);
+        charInputGUI.add(cboClasses);
         charInputGUI.add(txtCharName);
-        charInputGUI.add(lblCharName);
-        charInputGUI.add(lblPlayerName);
-        charInputGUI.add(lblBackground);
+        charInputGUI.add(txtPlayerName);
         charInputGUI.add(txtBackground);
         charInputGUI.add(txtAlignment);
         charInputGUI.add(txtLevel);
@@ -93,6 +90,9 @@ public class CharInfoInput extends JPanel implements ActionListener {
         charInputGUI.add(txtCharisma);
         charInputGUI.add(txtMoveSpeed);
         charInputGUI.add(txtMaxHP);
+        charInputGUI.add(lblCharName);
+        charInputGUI.add(lblPlayerName);
+        charInputGUI.add(lblBackground);
         charInputGUI.add(lblAlignment);
         charInputGUI.add(lblRace);
         charInputGUI.add(lblLevel);
@@ -105,38 +105,38 @@ public class CharInfoInput extends JPanel implements ActionListener {
         charInputGUI.add(lblMoveSpeed);
         charInputGUI.add(lblMaxHP);
         //Placing the components on the GUI window
-        confirmButton.setBounds(30, 420, 200, 20);
-        titleLabel.setBounds(30, 15, 200, 25);
-        lblCharName.setBounds (30, 50, 100, 25);
-        lblPlayerName.setBounds (30, 75, 100, 25);
-        lblBackground.setBounds (30, 100, 100, 25);
-        lblAlignment.setBounds (30, 125, 100, 25);
-        lblRace.setBounds (30, 150, 100, 25);
-        lblLevel.setBounds (30, 175, 100, 25);
-        lblStrength.setBounds (30, 200, 100, 25);
-        lblDexterity.setBounds (30, 225, 100, 25);
-        lblConstitution.setBounds (30, 250, 100, 25);
-        lblIntelligence.setBounds (30, 275, 100, 25);
-        lblWisdom.setBounds (30, 300, 100, 25);
-        lblCharisma.setBounds (30, 325, 100, 25);
-        lblMoveSpeed.setBounds (30, 350, 100, 25);
-        lblMaxHP.setBounds (30, 375, 100, 25);
-        txtCharName.setBounds (130, 50, 100, 25);
-        txtPlayerName.setBounds (130, 75, 100, 25);
-        txtBackground.setBounds (130, 100, 100, 25);
-        txtAlignment.setBounds (130, 125, 100, 25);
-        txtLevel.setBounds (130, 175, 100, 25);
-        txtDexterity.setBounds (130, 225, 100, 25);
-        txtStrength.setBounds (130, 200, 100, 25);
-        txtRace.setBounds (130, 150, 100, 25);
-        txtConstitution.setBounds (130, 250, 100, 25);
-        txtWisdom.setBounds (130, 300, 100, 25);
-        txtIntelligence.setBounds (130, 275, 100, 25);
-        txtCharisma.setBounds (130, 325, 100, 25);
-        txtMoveSpeed.setBounds (130, 350, 100, 25);
-        txtMaxHP.setBounds (130, 375, 100, 25);
+        lblTitle.setBounds(30, 15, 200, 25);
+        cboClasses.setBounds(30,50,200,25);
+        lblCharName.setBounds (30, 75, 100, 25);
+        lblPlayerName.setBounds (30, 100, 100, 25);
+        lblBackground.setBounds (30, 125, 100, 25);
+        lblAlignment.setBounds (30, 150, 100, 25);
+        lblRace.setBounds (30, 175, 100, 25);
+        lblLevel.setBounds (30, 200, 100, 25);
+        lblStrength.setBounds (30, 225, 100, 25);
+        lblDexterity.setBounds (30, 250, 100, 25);
+        lblConstitution.setBounds (30, 275, 100, 25);
+        lblIntelligence.setBounds (30, 300, 100, 25);
+        lblWisdom.setBounds (30, 325, 100, 25);
+        lblCharisma.setBounds (30, 350, 100, 25);
+        lblMoveSpeed.setBounds (30, 375, 100, 25);
+        lblMaxHP.setBounds (30, 400, 100, 25);
+        txtCharName.setBounds (130, 75, 100, 25);
+        txtPlayerName.setBounds (130, 100, 100, 25);
+        txtBackground.setBounds (130, 125, 100, 25);
+        txtAlignment.setBounds (130, 150, 100, 25);
+        txtRace.setBounds (130, 175, 100, 25);
+        txtLevel.setBounds (130, 200, 100, 25);
+        txtStrength.setBounds (130, 225, 100, 25);
+        txtDexterity.setBounds (130, 250, 100, 25);
+        txtConstitution.setBounds (130, 275, 100, 25);
+        txtIntelligence.setBounds (130, 300, 100, 25);
+        txtWisdom.setBounds (130, 325, 100, 25);
+        txtCharisma.setBounds (130, 350, 100, 25);
+        txtMoveSpeed.setBounds (130, 375, 100, 25);
+        txtMaxHP.setBounds (130, 400, 100, 25);
         //Adding ActionListener to the confirm button
-        confirmButton.addActionListener(this);
+        //confirmButton.addActionListener(this);
     }
 
     public void openCharInput()
@@ -144,14 +144,19 @@ public class CharInfoInput extends JPanel implements ActionListener {
         charInputGUI.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e)
-    {
-        //Returning a String from the ActionEvent to find which button was pressed
-        String s = e.getActionCommand();
-        if(s.equals("Confirm Character Input"))
-        {
-            System.out.println("Character Input");
-        }
+    public void addConfirmButton(JButton confirmButton) {
+        charInputGUI.add(confirmButton);
+        confirmButton.setBounds(30, 440, 200, 20);
+        charInputGUI.repaint();
+    }
 
-    }// End actionPerformed
+    public String getCharClass()
+    {
+        return (String) cboClasses.getSelectedItem();
+    }
+
+    public String getCharName()
+    {
+        return txtCharName.getText();
+    }
 }//End CharInfoInput class
--- src/CharacterMenu.java
+++ src/CharacterMenu.java
@@ -5,49 +5,57 @@ import java.lang.Character;
 import java.util.LinkedList;
 
 public class CharacterMenu implements ActionListener {
-
     //Creating the main gui window, and gui components
     private static JFrame guiWindow = new JFrame("Character Control");
     private static JMenuBar guiMenuBar = new JMenuBar();
     private static JMenu charList = new JMenu("Characters");
-    private static JLabel charInfoLabel = new JLabel("");
     private static JPanel charInfo = new JPanel();
+    private static JLabel charInfoLabel = new JLabel("");
     //Creating an LinkedList to store character information
     public LinkedList<CharSheet> characters = new LinkedList<CharSheet>();
     //Creates an object of the character info input menu
     public CharInfoInput charInput = new  CharInfoInput();
+    //Creates variables for character info input
+    private String charClass="";
+    private String charName="";
+    private String playerName="";
+    private String background="";
+    private String alignment="";
+    private String race="";
+    private int level=0;
+    private int strength=0;
+    private int dexterity=0;
+    private int constitution=0;
+    private int intelligence=0;
+    private int wisdom=0;
+    private int charisma=0;
+    private int moveSpeed=0;
+    private int maxHP=0;
 
     public CharacterMenu()
     {
         //Setting the properties of the JFrame window
         guiWindow.setSize(1000,800);
         //Creating an "Add Character" menu dropdown
-        JMenu addChar = new JMenu("Add Character");
-        //Creating menu items for the dropdown
-        JMenuItem addWizard = new JMenuItem("New Wizard");
-        JMenuItem addSorcerer = new JMenuItem("New Sorcerer");
-        JMenuItem addBarbarian = new JMenuItem("New Barbarian");
-        JMenuItem addRogue = new JMenuItem("New Rogue");
-        JMenuItem addCleric = new JMenuItem("New Cleric");
-        //Adding the items to the dropdown menu
-        addChar.add(addWizard);
-        addChar.add(addSorcerer);
-        addChar.add(addRogue);
-        addChar.add(addBarbarian);
-        addChar.add(addCleric);
-        //Adding the action listener to the dropdown items
-        addWizard.addActionListener(this);
-        addSorcerer.addActionListener(this);
-        addBarbarian.addActionListener(this);
-        addRogue.addActionListener(this);
-        addCleric.addActionListener(this);
+        JMenuItem addChar = new JMenuItem("New Character");
+        //Adding the action listener to the menu
+        addChar.addActionListener(this);
         //Placing the menu bar on the JFrame, adding the dropdown menus to the menu bar and setting the JFrame to visible
         guiWindow.setJMenuBar(guiMenuBar);
-        guiMenuBar.add(addChar);
         guiMenuBar.add(charList);
+        guiMenuBar.add(addChar);
         //Adding character info display to the GUI
         charInfo.add(charInfoLabel);
         guiWindow.add(charInfo, BorderLayout.WEST);
+        //Creates a confirm button to add to the hidden character input menu
+        JButton confirmButton = new JButton("Confirm Character Input");
+        //Adds action listener to this button
+        confirmButton.addActionListener(this);
+        //Adds character input panel to the GUI
+        guiWindow.add(charInput);
+        //Adds the confirm button onto the character menu panel
+        charInput.addConfirmButton(confirmButton);
+
     }// End menu constructor
 
     public void openCharMenu()
@@ -60,36 +68,55 @@ public class CharacterMenu implements ActionListener {
     {
         //Returning a String from the ActionEvent to find which button was pressed
         String s = e.getActionCommand();
-        System.out.println(s.substring(0,3));
+        System.out.println(s);
         //If button pressed is to add a new character
-        if(s.substring(0,3).equals("New"))
+        if(s.equals("New Character"))
         {
-            //Creating charInfo string for later use
-            String charInfo = "";
+            //Opens the charInput gui panel
+            charInput.openCharInput();
+        }
 
-            //If chosen class is Wizard
-            if(s.equals("New Wizard"))
+        //If button pressed starts with a digit (character return buttons all do)
+        if(Character.isDigit(s.charAt(0)))
+        {
+            //Returns the position of the character in the characters array based on its button text
+            int number = (Character.getNumericValue(s.charAt(0)) - 1);
+            //Sets text of the character info label
+            charInfoLabel.setText(characters.get(number).toString());
+            //Repaints the GUI window to account for the changes in the info label
+            guiWindow.repaint();
+        }
+
+        //If button pressed is to confirm character info input
+        if(s.equals("Confirm Character Input"))
+        {
+            //Action handling for when the user confirms character input on the character input box
+            //Creates variables for storing inputted values
+            charClass = charInput.getCharClass();
+            charName = "";
+            playerName = "";
+            background = "";
+            alignment = "";
+            race = "";
+            level = 0;
+            strength = 0;
+            dexterity = 0;
+            constitution = 0;
+            intelligence = 0;
+            wisdom = 0;
+            charisma = 0;
+            moveSpeed = 0;
+            maxHP = 0;
+
+            //Creates the charInfo string to add to the character menu dropdown
+            String charInfo = (characters.size()) + " - " + charClass + " - " + charName;
+            //If statement handling for different character classes
+            if(charClass.equals("Wizard"))
             {
-                charInput.openCharInput();
-                //Reads in character name from user --- TEMPORARY --- to be replaced with gui allowing all data to be input at once
-                String charName = JOptionPane.showInputDialog("Enter Wizard Name");
-                //Creates new Wizard --- TEMPORARY --- Currently using hard coded values, see above comment
-                Wizard wizard = new Wizard(charName, "PlayerName", "Background", "Alignment", "Race", 5, 10, 12, 12, 18, 16, 14, 30, 20);
+                Wizard wizard = new Wizard(charName, playerName, background, alignment, race, level, strength, dexterity, constitution, intelligence, wisdom, charisma, moveSpeed, maxHP);
                 characters.add(wizard);
                 //Assigning charInfo string and charArrayPos int for storage in arrays to use to retrieve character info
                 charInfo = (characters.size()) + " - Wizard - " + charName;
-            }
-
-            //If chosen class is Sorcerer
-            if(s.equals("New Sorcerer"))
-            {
-                //Reads in character name from user --- TEMPORARY --- to be replaced with gui allowing all data to be input at once
-                String charName = JOptionPane.showInputDialog("Enter Sorcerer Name");
-                //Creates new Wizard --- TEMPORARY --- Currently using hard coded values, see above comment
-                Sorcerer sorcerer = new Sorcerer(charName, "PlayerName", "Background", "Alignment", "Race", 5, 10, 12, 12, 18, 16, 14, 30, 20);
-                characters.add(sorcerer);
-                //Assigning charInfo string and charArrayPos int for storage in arrays to use to retrieve character info
-                charInfo = (characters.size()) + " - Sorcerer - " + charName;
             }
 
             //Creating a new JMenuItem with the character info corresponding to the new character
@@ -99,22 +126,6 @@ public class CharacterMenu implements ActionListener {
             //Adding the new menu item to the charList JMenu
             charList.add(character);
             //Repainting the window so the new character appears in the character list
-            guiWindow.repaint();
-        }
-
-        //If button pressed starts with a digit (character return buttons all do)
-        if(Character.isDigit(s.charAt(0)))
-        {
-            System.out.println(s);
-            System.out.println("\nNumeric Value of Char : " + (Character.getNumericValue(s.charAt(0)) - 1));
-            //Returns the position of the character in the characters array based on its button text
-            int number = (Character.getNumericValue(s.charAt(0)) - 1);
-
-            System.out.println("Char Array Size: " + characters.size());
-            System.out.println("Char Name: " + characters.get(number).getCharName());
-            //Sets text of the character info label
-            charInfoLabel.setText(characters.get(number).toString());
-            //Repaints the GUI window to account for the changes in the info label
             guiWindow.repaint();
         }
     }// End actionPerformed
